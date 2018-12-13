62d05d1fdf40ed2d6c5200e30d1e0bad70003f17
horse <littlehorseboy@gmail.com>
2018-12-13 08:36:55 +0800

    fix: querySelector 沒打 #

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import Title from '../Title/Title.jsx';
+import PropTypes from 'prop-types';
+import Title from '../Title/Title.jsx';
 
 /* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
 
 class Main extends React.Component {
   render() {
-    return <h1>asd</h1>;
+    return <Title title='Hello' />;
   }
 }
 
-export { Main };
+Main.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Main;
